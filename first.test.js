940cd1e7d06aba15037fe27d68f432ab6f4b35dc
Rom4ig <romses2015@gmail.com>
2020-01-24 14:54:21 +0300

    added post, get, patch, put, delete requests

--- first.test.js
+++ first.test.js
@@ -1,11 +1,60 @@
 const axios = require('./axios');
 const expect = require('chai').expect;
 const validate = require('./validate');
-const schema = require('./schema');
-
-test('Some test', async () => {
-    const res = await axios.get('api/unknown');
-    let valid = validate(schema, res);
+const schemaGET = require('./schemaGETunknown.json');
+const schemaPOST = require('./schemaPOSTuser.json');
+const schemaPUT = require('./schemaPUTuser.json');
+let id;
+test('Get test', async () => {
+    let res = await axios.get('api/unknown');
+    console.log('GET');
+    let valid = validate(schemaGET, res);
     expect(valid).to.be.true;
 });
+
+test('Post test', async () => {
+    let data = {
+        "name": "morpheus",
+        "job": "leader"
+    };
+    let res = await axios.post('api/users', data);
+    console.log('POST');
+    console.log(res.data);
+    let valid = validate(schemaPOST, res);
+    expect(valid).to.be.true;
+});
+
+test('Put test', async () => {
+    let data = {
+        "name": "morpheus",
+        "job": "zion resident"
+    };
+    let res = await axios.put('api/users/2', data);
+    console.log('PUT');
+    console.log(res.data);
+    let valid = validate(schemaPUT, res);
+    expect(valid).to.be.true;
+});
+
+test('Delete test', async () => {
+
+    let res = await axios.delete('api/users/2');
+    console.log('DELETE');
+    //console.log(res);
+    expect(res.status).to.equal(204);
+});
+
+test('Patch test', async () => {
+    let data = {
+        "name": "ivan",
+        "job": "manager"
+    };
+    let res = await axios.put('api/users/2', data);
+    console.log('PATCH');
+    console.log(res.data);
+    let valid = validate(schemaPUT, res);
+    expect(valid).to.be.true;
+});
+
+
 
