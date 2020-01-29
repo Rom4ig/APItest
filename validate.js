d67ce6b55eb0ae1ed9c074f796255091ff0b3bf3
Rom4ig <romses2015@gmail.com>
2020-01-29 16:51:07 +0300

    Validate schemas before run test.

--- validate.js
+++ validate.js
@@ -1,19 +1,12 @@
-const schemaGET = require('./schemas/schemaGETunknown.json');
-const schemaPOST = require('./schemas/schemaPOSTuser.json');
-const schemaPUT = require('./schemas/schemaPUTuser.json');
-const schema = require('./schemas/schema.json');
 const AJV = require('ajv');
 const ajv = new AJV({
     schemas: [
         require('./schemas/definitions.json')
     ]
 });
-ajv.addSchema(schemaGET, './schemas/schemaGETunknown.json');
-ajv.addSchema(schemaPOST, './schemas/schemaPOSTuser.json');
-ajv.addSchema(schemaPUT, './schemas/schemaPUTuser.json');
-ajv.addSchema(schema, './schemas/schema.json');
 
 module.exports = function (schemaName, json) {
+    ajv.addSchema(require(schemaName), schemaName);
     let valid = ajv.validate(schemaName,json);
     if (!valid) console.log(ajv.errors);
     return valid;
